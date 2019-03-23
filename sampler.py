6b0ba8a7f94ae73c04cfc14f5615d6fd189b1433
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-03-23 17:51:32 +0100

    Update sampler.py

--- sampler.py
+++ sampler.py
@@ -49,7 +49,8 @@ amps3 = functions.readAmps( adc, 3, config)
     
 #conn.close()
 #print("Database closed")
-
+print("Total Consumption:",(amps0+amps1+amps2)*235)
+print("Total Production :",(amps3)*235)
 dt = datetime.datetime.now()
 print("===== sampler.py exiting at ", dt.isoformat())
 
