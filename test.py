0a14b938bfba30d07bbd81c11cecd706e5ce4c6f
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-03-23 23:31:19 +0100

    Update test.py

--- test.py
+++ test.py
@@ -11,6 +11,14 @@ except IOError as e:
   exit()
   
 r = reader.Reader(config)
-r.read()
-print("Current:",r.getLastCurrent())
-print("Power:",r.getLastPower())
+r.read(0)
+consphase1 = r.getLastPower()
+r.read(1)
+consphase2 = r.getLastPower()
+r.read(2)
+consphase3 = r.getlastPower()
+r.read(3)
+prod = r.getlastPower()
+
+print("Total consumption :",consphase1+consphase2+consphase3)
+print("Total production  :",prod)
