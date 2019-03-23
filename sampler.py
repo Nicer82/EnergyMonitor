4657b420bfe10fcb116b48a6cf82b204463a3c91
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-03-23 17:47:12 +0100

    Update sampler.py

--- sampler.py
+++ sampler.py
@@ -14,7 +14,7 @@ print("===== sampler.py starting at ", dt.isoformat())
 try:
     with open('./settings.json') as json_data:
         config = json.load(json_data)
-        print("Configuration read")
+#        print("Configuration read")
 except IOError as e:
   print("Unable to open configuration file:", e)
   exit()
@@ -24,29 +24,31 @@ adc = Adafruit_ADS1x15.ADS1115()
 
 try:
     conn = sqlite3.connect(config["Database"])
-    print('Connected to database.')
+#    print('Connected to database.')
 except Exception as e:
     print("Unable to open database: ", e)
     exit()
 
-# Read channels 0 and 1
+# Read channels 0 - 3
 amps0 = functions.readAmps( adc, 0, config)
 amps1 = functions.readAmps( adc, 1, config)
+amps2 = functions.readAmps( adc, 2, config)
+amps3 = functions.readAmps( adc, 3, config)
 
 #Save to the database
-try:
-    c = conn.cursor()
-    dt = datetime.datetime.now()
-    sql = "INSERT INTO ReadingData (TimeStamp, Channel, Current, TransmitDate) VALUES ('{0}', {1}, {2}, NULL)".format( dt.isoformat(), 0, amps0)
-    print( "Saving to database" )
-    c.execute(sql)
-    conn.commit()
-except sqlite3.Error as e:
-    print("Error writing to database: ", e)
-    exit()
+#try:
+#    c = conn.cursor()
+#    dt = datetime.datetime.now()
+#    sql = "INSERT INTO ReadingData (TimeStamp, Channel, Current, TransmitDate) VALUES ('{0}', {1}, {2}, NULL)".format( dt.isoformat(), 0, amps0)
+#    print( "Saving to database" )
+#    c.execute(sql)
+#    conn.commit()
+#except sqlite3.Error as e:
+#    print("Error writing to database: ", e)
+#    exit()
     
-conn.close()
-print("Database closed")
+#conn.close()
+#print("Database closed")
 
 dt = datetime.datetime.now()
 print("===== sampler.py exiting at ", dt.isoformat())
