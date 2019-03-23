19444f34fdec36567fa5892dc220350339f184ac
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-03-24 00:02:17 +0100

    Update test.py

--- test.py
+++ test.py
@@ -3,16 +3,20 @@ import reader
 import json
 import sqlite3
 
-#Read configuration
-try:
-    with open('./settings.json') as json_data:
-        config = json.load(json_data)
+# Read configuration
+with open('./settings.json') as json_data:
+    config = json.load(json_data)
 
+# Infinite loop
+while True:
+try
+    # Set worker variables and open the DB
     conn = sqlite3.connect(config["Database"])
     c = conn.cursor()
     sql = "INSERT INTO ReadingData VALUES ({0},{1},{2},{3})"
     r = reader.Reader(config)
 
+    # Read the channels
     r.read(0)
     c.execute(sql.format(r.getLastStart(),0,r.getLastPower(),'NULL'))
     r.read(1)
@@ -22,6 +26,7 @@ try:
     r.read(3)
     c.execute(sql.format(r.getLastStart(),3,r.getLastPower(),'NULL'))
 
+    # Close the DB
     conn.commit()
     conn.close()
 except Exception as e:
