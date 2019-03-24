4ac62b566a7159744987ec8f41f2238730a701c4
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-03-24 19:22:45 +0100

    Update uploader.py

--- uploader.py
+++ uploader.py
@@ -1,6 +1,17 @@
 import mysql.connector
+import sqlite3
 
-cnx = mysql.connector.connect(user='EnergyMonitor', password='Energy4All!',
+connServer = mysql.connector.connect(user='EnergyMonitor', password='Energy4All!',
                               host='192.168.1.2',
                               database='EnergyMonitor')
-cnx.close()
+connLocal = sqlite3.connect(config["Database"])
+connLocal.row_factory = sqlite3.Row
+curLocal = connLocal.cursor()
+
+curLocal.execute("SELECT TimeStamp,Channel,ConsumptionWh,PowerMinW,PowerMaxW,PowerAvgW,PowerStDevW,Measurements FROM ReadingData WHERE UploadedTimeStamp IS NULL")
+
+for row in curLocal:
+  print(r['TimeStamp'],r['Channel'])
+    
+connLocal.close()
+connServer.close()
