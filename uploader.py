fa9b8c13b355f7747d53d897608987137ddcac0e
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-03-24 23:04:45 +0100

    Update uploader.py

--- uploader.py
+++ uploader.py
@@ -13,11 +13,13 @@ with open('/home/pi/EnergyMonitor/config.json') as json_data:
 while(True):
     try:
         # Set up DB connections
+        print("connect server")
         connServer = mysql.connector.connect(user=config["Uploader"]["User"],
                                              password=config["Uploader"]["Password"],
                                              host=config["Uploader"]["Host"],
                                              port=config["Uploader"]["Port"],
                                              database=config["Uploader"]["Database"])
+        print("connect local")
         connLocal = sqlite3.connect(config["Logger"]["Database"])
         connLocal.row_factory = sqlite3.Row
 
@@ -25,10 +27,12 @@ while(True):
         uploadedTimeStamp = time.time()
         curServer = connServer.cursor()
         curLocal = connLocal.cursor()
+        print("execute local fetch")
         curLocal.execute("SELECT TimeStamp,Channel,ConsumptionWh,PowerMinW,PowerMaxW,PowerAvgW,PowerStDevW,Measurements FROM ReadingData WHERE UploadedTimeStamp IS NULL")
 
         # Loop through rows from local DB and insert them into the server DB
         for row in curLocal:
+            print("execute server insert")
             curServer.execute("INSERT INTO ReadingData VALUES ('{0}','{1}',{2},{3},{4},{5},{6},{7},{8},'{9}')".format(datetime.fromtimestamp(row['TimeStamp']),
                                                                                                                 socket.gethostname(),
                                                                                                                 row['Channel'],
@@ -39,14 +43,20 @@ while(True):
                                                                                                                 row['PowerStDevW'],
                                                                                                                 row['Measurements'],
                                                                                                                 datetime.fromtimestamp(uploadedTimeStamp)))
+            print("execute local delete")
             curLocal.execute("DELETE FROM ReadingData WHERE Timestamp = {0} AND Channel = {1}".format(row['TimeStamp'],
                                                                                                       row['Channel']))
+        print("server commit")
         connServer.commit()
+        print("server close")
         connServer.close()
+        print("local commit")
         connLocal.commit()
+        print("local close")
         connLocal.close()    
     except Exception as e:
         print("An error occurred:", e)
     finally:
         # Wait x seconds to upload next set of data
+        print("sleep")
         time.sleep(config["Uploader"]["UploadInterval"])
