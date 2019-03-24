b8e1ad2322e46e29f5b8d097cc9e27cea0c1fe8d
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-03-24 19:25:10 +0100

    Update uploader.py

--- uploader.py
+++ uploader.py
@@ -1,13 +1,19 @@
 #import mysql.connector
 import sqlite3
+import json
 
+# Read configuration
+with open('/home/pi/EnergyMonitor/config.json') as json_data:
+    config = json.load(json_data)
+
+# Set up DB connection    
 #connServer = mysql.connector.connect(user='EnergyMonitor', password='Energy4All!',
 #                              host='192.168.1.2',
 #                              database='EnergyMonitor')
 connLocal = sqlite3.connect(config["Database"])
 connLocal.row_factory = sqlite3.Row
-curLocal = connLocal.cursor()
 
+curLocal = connLocal.cursor()
 curLocal.execute("SELECT TimeStamp,Channel,ConsumptionWh,PowerMinW,PowerMaxW,PowerAvgW,PowerStDevW,Measurements FROM ReadingData WHERE UploadedTimeStamp IS NULL")
 
 for row in curLocal:
