a425d8cc676cdc58280d1361fad92a8981e85f6f
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-03-24 19:54:20 +0100

    Update reader.py

--- reader.py
+++ reader.py
@@ -13,32 +13,6 @@ class Reader():
     self._adc = Adafruit_ADS1x15.ADS1115()
     self._config = config
     
-  # Calculate the average value
-  def averagemax(self,values):
-      maxvalues = []
-      prev = 0.0
-      prevmax = -1.0
-      startcollection = False
-
-      for value in values:
-          # Has the AC sinus passed the zero line?
-          if(value*prev < 0.0):
-              if(startcollection):
-                  maxvalues.append(prevmax)
-                  prevmax = 0.0
-              else:
-                  startcollection = True
-
-          # Only set prevmax when zero line is passed for the first time
-          if(startcollection and abs(value) > prevmax):
-              prevmax = abs(value)
-
-          prev = value
-
-      avgmax = self.average(maxvalues)
-
-      return avgmax
-  
   def rootmeansquare(self,values):
     ssq = 0.0
     sum = 0.0
@@ -58,23 +32,15 @@ class Reader():
     
     return rms
 
-  # Calculate the average value
-  def average(self,values):
-      sum = 0
-      for value in values:
-          sum = sum + value
-      avg = sum/len(values)    
-      return avg
-
   # Read an ADC channel with the specified gain at max rate for 1/4 second
   def readChannel(self, chan):
       values = []
       
-      self._adc.start_adc(chan, gain=self._config["Sampler"]["gain"], data_rate=860)
+      self._adc.start_adc(chan, gain=self._config["Reader"]["Gain"], data_rate=self._config["Reader"]["DataRate"])
       
       # Sample for 1/4 second
       self._lastStart = time.time()
-      while (time.time() - self._lastStart) <= 0.25:
+      while (time.time() - self._lastStart) <= self._config["Reader"]["ReadTime"]:
           # Read the last ADC conversion value and print it out.
           value = float(self._adc.get_last_result())
           values.append(value)
@@ -88,8 +54,6 @@ class Reader():
   # Read and compute the amperage on the specified channel
   def read(self, chan=0):
       self._chan = chan
-      SUBSTRACTOR = self._config["Sampler"]["substractor"]
-      FACTOR = self._config["Sampler"]["factor"]
 
       try:
           values = self.readChannel(chan)
@@ -101,14 +65,14 @@ class Reader():
           exit()
 
       avgmax = self.rootmeansquare(values)
-      amps = (avgmax - SUBSTRACTOR) * FACTOR
+      amps = (avgmax - self._config["Reader"]["Substractor"]) * self._config["Reader"]["Factor"]
       
       # Suppress values below zero
       if(amps < 0.0):
           amps = 0.0
       
       self._lastCurrent = amps
-      self._lastPower = amps * self._config["Voltage"]
+      self._lastPower = amps * self._config["Reader"]["Voltage"]
 
       return
     
