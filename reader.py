5d9186f1be2ff5323dc2d1af4ca3657ff7ab6886
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-03-25 15:04:27 +0100

    Update reader.py

--- reader.py
+++ reader.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
+# -------------------------------------------------------------------------
+# Program: Class that handles reading out current sensors over the ADS1115
+#
+# Copyright (C) 2019 Bjorn Douchy
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License at https://www.gnu.org/licenses
+#    for more details.
+# -------------------------------------------------------------------------
 import time
 import math
 import Adafruit_ADS1x15
