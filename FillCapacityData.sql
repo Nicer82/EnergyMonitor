7148b78c0967d67d7969f6338626ef4fcc13f7f9
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-03-27 15:57:01 +0100

    Add files via upload

---
+++ FillCapacityData.sql
@@ -0,0 +1,64 @@
+SET time_zone='+00:00';
+SET @CapInterval := 3600;
+SELECT @TotalChannels := COUNT(*) FROM ChannelSetup;
+SELECT @LastCapTimeStamp := CAST(COALESCE(MAX(TimeStamp),'1970-01-01') AS DATETIME) FROM CapacityData;
+SELECT @LastReadTimeStamp := rd.TimeStamp
+FROM ReadingData rd
+	INNER JOIN ChannelSetup cs
+    ON cs.Device = rd.Device
+    AND cs.Channel = rd.Channel
+WHERE rd.TimeStamp > @LastCapTimeStamp 
+GROUP BY rd.TimeStamp
+HAVING COUNT(*) = @TotalChannels
+ORDER BY rd.TimeStamp DESC
+LIMIT 1;
+
+INSERT INTO CapacityData (TimeStamp,Type,InstallationWh,GridWh)
+WITH InstallationData AS
+(
+	SELECT
+		rd.TimeStamp AS TimeStamp,
+		cs.Type AS Type,
+		SUM(rd.ConsumptionWh) AS InstallationWh
+	FROM ReadingData AS rd
+		INNER JOIN ChannelSetup AS cs
+		ON cs.Device = rd.Device
+		AND cs.Channel = rd.Channel
+	WHERE rd.TimeStamp >= FROM_UNIXTIME((UNIX_TIMESTAMP(@LastCapTimeStamp) DIV @CapInterval + 1) * @CapInterval)
+	AND rd.TimeStamp < FROM_UNIXTIME(UNIX_TIMESTAMP(@LastReadTimeStamp) DIV @CapInterval * @CapInterval)
+	GROUP BY 
+		rd.TimeStamp, 
+		cs.Type
+),
+GridData AS
+(
+	SELECT 
+		TimeStamp,
+		SUM(
+			CASE Type
+				WHEN 'Production' THEN -InstallationWh
+                WHEN 'Consumption' THEN InstallationWh
+			END
+		) AS GridWh
+	FROM InstallationData
+    GROUP BY TimeStamp
+)
+SELECT
+	FROM_UNIXTIME(UNIX_TIMESTAMP(i.TimeStamp) DIV @CapInterval * @CapInterval),
+    i.Type,
+    SUM(i.InstallationWh),
+    SUM(
+		CASE
+			WHEN i.Type = 'Production' AND g.GridWh > 0 THEN 0.0
+			WHEN i.Type = 'Production' AND g.GridWh < 0 THEN g.GridWh
+			WHEN i.Type = 'Consumption' AND g.GridWh > 0 THEN g.GridWh
+			WHEN i.Type = 'Consumption' AND g.GridWh  < 0 THEN 0.0
+			ELSE 0.0
+		END
+	)
+FROM InstallationData AS i
+	INNER JOIN GridData AS g
+    ON g.TimeStamp = i.TimeStamp
+GROUP BY
+	FROM_UNIXTIME(UNIX_TIMESTAMP(i.TimeStamp) DIV @CapInterval * @CapInterval),
+    i.Type;
