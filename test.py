6b42b5a51d1c0a5e51a588cad88588ea8ead3d5e
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-04-03 16:57:46 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,11 +2,11 @@ import currentreader
 
 reader = currentreader.CurrentReader(voltage=230)
 
-reader.readChannel(chan=0,ampFactor=0.0026,ampExponent=1.0206,ampMinimum=0.015)
+reader.readChannel(chan=0,ampFactor=0.0026,ampExponent=1.0206,ampMinimum=0.020)
 print("Power: {0}".format(reader.lastPower()))
-reader.readChannel(chan=1,ampFactor=0.0026,ampExponent=1.0206,ampMinimum=0.015)
+reader.readChannel(chan=1,ampFactor=0.0026,ampExponent=1.0206,ampMinimum=0.020)
 print("Power: {0}".format(reader.lastPower()))
-#reader.readChannel(chan=2,ampFactor=0.0026,ampExponent=1.0206,ampMinimum=0.015)
+#reader.readChannel(chan=2,ampFactor=0.0026,ampExponent=1.0206,ampMinimum=0.020)
 #print("Power: {0}".format(reader.lastPower()))
-#reader.readChannel(chan=3,ampFactor=0.0026,ampExponent=1.0206,ampMinimum=0.015)
+#reader.readChannel(chan=3,ampFactor=0.0026,ampExponent=1.0206,ampMinimum=0.020)
 #print("Power: {0}".format(reader.lastPower()))
