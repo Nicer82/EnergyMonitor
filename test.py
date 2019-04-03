7eab607c04a4cf736e9190b3a937231cd9c4a7eb
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-04-03 11:06:14 +0200

    Update test.py

--- test.py
+++ test.py
@@ -7,8 +7,23 @@ import array
 import json
 import statistics
 
-adc = Adafruit_ADS1x15.ADS1115()
+def rootmeansquare(values):
+    # RMS = SQUARE_ROOT((values[0]² + values[1]² + ... + values[n]²) / LENGTH(values))
+    sumsquares = 0.0
+    avg = statistics.mean(values)
+    
+    for value in values:
+        sumsquares = sumsquares + (value-avg)**2  # substract avg from value to correct the values and make sure we have the 0V line on the avg
 
+    if len(values) == 0:
+        rms = 0.0
+    else:
+        rms = math.sqrt(float(sumsquares)/len(values))
+    
+    return rms
+
+adc = Adafruit_ADS1x15.ADS1115()
+values = []
 start = time.time()
 end = start + 1
 
@@ -16,6 +31,8 @@ adc.start_adc(channel=0, gain=1, data_rate=860)
 
 while (time.time() < end):
     value = adc.get_last_result()
-    print(value)
+    values.append(value)
 
 adc.stop_adc()
+
+print(rootmeansquare(values))
