cd2d51ef4fa1a6093783b0579b90a4d1c26a91db
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-04-18 15:02:54 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ import currentreader
 reader = currentreader.CurrentReader(voltage=1)
 
 reader.readChannel(chan=3,ampFactor=1,ampExponent=1,ampMinimum=0)
-print("start")
+print("===========================================================> start")
 reader.readChannel(chan=3,ampFactor=1,ampExponent=1,ampMinimum=0)
