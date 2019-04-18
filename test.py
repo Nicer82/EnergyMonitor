e342bd6ffdb5b48a47c20ec7a5186e0d6e8eb5fb
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-04-18 15:47:59 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ import currentreader
 
 reader = currentreader.CurrentReader(voltage=230)
 
-reader.readChannel(chan=3,ampFactor=0.0025,ampExponent=1,ampMinimum=0)
+reader.readChannel(chan=3,ampFactor=0.002818,ampExponent=1,ampMinimum=0)
 print(reader.lastPower())
 
