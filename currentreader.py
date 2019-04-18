c998a450ee1052eb331c4e1c785611276b8d9377
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-04-18 14:54:40 +0200

    Update currentreader.py

--- currentreader.py
+++ currentreader.py
@@ -80,6 +80,7 @@ class CurrentReader():
 
         while (time.time() < self._lastEnd):
             val = self._adc.get_last_result()
+            print(val)
             readValues.append(val)
             
         self._adc.stop_adc()
