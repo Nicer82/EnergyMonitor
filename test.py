fcf5e837c04de7087a5a52a302a561913806c08a
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-04-18 10:14:54 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,11 +3,12 @@ import currentreader
 reader = currentreader.CurrentReader(voltage=1)
 
 for i in range(10):
-  reader.readChannel(chan=0,ampFactor=1,ampExponent=1,ampMinimum=0)
-  print(reader.lastPower())
-  reader.readChannel(chan=1,ampFactor=1,ampExponent=1,ampMinimum=0)
-  print(reader.lastPower())
-  reader.readChannel(chan=2,ampFactor=1,ampExponent=1,ampMinimum=0)
-  print(reader.lastPower())
-  reader.readChannel(chan=3,ampFactor=1,ampExponent=1,ampMinimum=0)
-  print(reader.lastPower())
+  reader.readChannel(chan=0,ampFactor=1,ampExponent=1,ampMinimum=0,cycle=i)
+  print("{0};{1};{2}".format(i,0,reader.lastPower()))
+  reader.readChannel(chan=1,ampFactor=1,ampExponent=1,ampMinimum=0,cycle=i)
+  print("{0};{1};{2}".format(i,1,reader.lastPower()))
+  reader.readChannel(chan=2,ampFactor=1,ampExponent=1,ampMinimum=0,cycle=i)
+  print("{0};{1};{2}".format(i,2,reader.lastPower()))
+  reader.readChannel(chan=3,ampFactor=1,ampExponent=1,ampMinimum=0,cycle=i)
+  print("{0};{1};{2}".format(i,3,reader.lastPower()))
+  
