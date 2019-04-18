a64a8f17fa11b7920155ce124e57af927c424eea
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-04-18 16:43:58 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,5 +4,5 @@ reader = currentreader.CurrentReader(voltage=230)
 print("Channel?")
 chan = int(input())
 reader.readChannel(chan,ampFactor=0.00282,ampExponent=1,ampMinimum=0)
-#print(reader.lastPower())
+print(reader.lastPower())
 
