88552147e0732e0550d0af4e3a3aae7161c6f97a
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-07-19 16:45:32 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import time
 
 _adc = Adafruit_ADS1x15.ADS1115()
 _adcChannel = 0
-_adcReadTime = 5 # how long do we read out the sine wave in seconds to get a reliable and stable readout
+_adcReadTime = 15 # how long do we read out the sine wave in seconds to get a reliable and stable readout
 _adcGain = 1 # gain factor, for reading lower currents
 _adcDataRate = 860 # samples per second
 
