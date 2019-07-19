15ba3856b1f5d00aac52c1d4726f85782d10e243
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-07-19 16:55:38 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,22 +2,36 @@ import time
 import board
 import busio
 import adafruit_ads1x15.ads1015 as ADS
+from adafruit_ads1x15.ads1x15 import Mode
 from adafruit_ads1x15.analog_in import AnalogIn
 
-# Create the I2C bus
-i2c = busio.I2C(board.SCL, board.SDA)
+# Data collection setup
+RATE = 3300
+SAMPLES = 1000
+
+# Create the I2C bus with a fast frequency
+i2c = busio.I2C(board.SCL, board.SDA, frequency=1000000)
 
 # Create the ADC object using the I2C bus
 ads = ADS.ADS1015(i2c)
 
 # Create single-ended input on channel 0
-chan = AnalogIn(ads, ADS.P0)
+chan0 = AnalogIn(ads, ADS.P0)
 
-# Create differential input between channel 0 and 1
-#chan = AnalogIn(ads, ADS.P0, ADS.P1)
+# ADC Configuration
+ads.mode = Mode.CONTINUOUS
+ads.data_rate = RATE
 
-print("{:>5}\t{:>5}".format('raw', 'v'))
+data = [None]*SAMPLES
 
-while True:
-    print("{:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
-    time.sleep(0.5)
+start = time.monotonic()
+
+# Read the same channel over and over
+for i in range(SAMPLES):
+    data[i] = chan0.value
+
+end = time.monotonic()
+total_time = end - start
+
+print("Time of capture: {}s".format(total_time))
+print("Sample rate requested={} actual={}".format(RATE, SAMPLES / total_time))
