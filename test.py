9b081fcc5b08e356caedfa59edcc5b1451b60597
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-07-19 16:57:52 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from adafruit_ads1x15.ads1x15 import Mode
 from adafruit_ads1x15.analog_in import AnalogIn
 
 # Data collection setup
-RATE = 860
+RATE = 490
 SAMPLES = 1000
 
 # Create the I2C bus with a fast frequency
