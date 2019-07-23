48bd478ca853b14a423f407b750c6bcd813a7129
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-07-23 11:11:13 +0200

    Update test.py

--- test.py
+++ test.py
@@ -16,32 +16,32 @@ i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
 ads = ADS.ADS1115(i2c)
 
 chan0 = AnalogIn(ads, ADS.P0)
-chan1 = AnalogIn(ads, ADS.P1)
 
 # ADC Configuration
 ads.mode = Mode.CONTINUOUS 
 ads.data_rate = RATE
 
 data0 = [None]*SAMPLES
-data1 = [None]*SAMPLES
 
 timebetweenreads = 1/RATE
-start = time.perf_counter()
-nextRead = start
+while(True):
+    print(
+    start = time.perf_counter()
+    nextRead = start
 
-# Current = measured voltage - 2.5 / burden resistor ohms * CT turn ratio
-# Read the same channel over and over
-for i in range(SAMPLES):
-    data0[i] = chan0.voltage
-    print(data0[i])
-    
-    nextRead += timebetweenreads
-    sleep = nextRead-time.perf_counter()
-    if sleep > 0:
-      time.sleep(sleep)
+    # Current = measured voltage - 2.5 / burden resistor ohms * CT turn ratio
+    # Read the same channel over and over
+    for i in range(SAMPLES):
+        data0[i] = chan0.voltage
+        print(data0[i])
 
-end = time.perf_counter()
-total_time = end - start
+        nextRead += timebetweenreads
+        sleep = nextRead-time.perf_counter()
+        if sleep > 0:
+          time.sleep(sleep)
 
-print("Time of capture: {}s".format(total_time))
-print("Sample rate requested={} actual={}".format(RATE, SAMPLES / total_time))
+    end = time.perf_counter()
+    total_time = end - start
+
+    print("Time of capture: {}s".format(total_time))
+    print("Sample rate requested={} actual={}".format(RATE, SAMPLES / total_time))
