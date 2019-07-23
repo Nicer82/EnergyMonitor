268967a38db65108c863a40a5c73b5dfca7f2215
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-07-23 08:44:42 +0200

    Update test.py

--- test.py
+++ test.py
@@ -16,10 +16,10 @@ i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
 ads = ADS.ADS1115(i2c)
 
 
-chan0 = AnalogIn(ads, ADS.P1)
-chan1 = AnalogIn(ads, ADS.P2)
-chan2 = AnalogIn(ads, ADS.P3)
-chan3 = AnalogIn(ads, ADS.P4)
+chan0 = AnalogIn(ads, ADS.P0)
+chan1 = AnalogIn(ads, ADS.P1)
+chan2 = AnalogIn(ads, ADS.P2)
+chan3 = AnalogIn(ads, ADS.P3)
 
 # ADC Configuration
 ads.mode = Mode.CONTINUOUS
