cffe55fe5237c81445d0d1b2a76a0f0f656af5e0
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-07-25 17:50:22 +0200

    Update testcv.py

--- testcv.py
+++ testcv.py
@@ -113,7 +113,7 @@ while(True):
     datac = readadc(chanc, startc)
     
     current = rootmeansquare(datac) / CT_BURDENRESISTOR * CT_TURNRATIO * flowdirection(datac,datav) * C_CALIBRATIONFACTOR
-    #print("Current: {} A, Reads: {}, VMin: {}, VMax: {}".format(current,len(datac),min(datac),max(datac)))
+    print("Current: {} A, Reads: {}, VMin: {}, VMax: {}".format(current,len(datac),min(datac),max(datac)))
     
     ### Power calculation
     power = current*voltage
