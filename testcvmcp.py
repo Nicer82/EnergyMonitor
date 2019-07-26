3df0b11c27ec6d4ed25906c4b61a8bd79cd33444
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-07-26 11:29:14 +0200

    Update testcvmcp.py

--- testcvmcp.py
+++ testcvmcp.py
@@ -97,9 +97,9 @@ print(2&3)
 print(3&3)
 
 for i in range(100):
-    r = spi.xfer2([1,8+0 << 4, 0,8+1 << 4,0],50000,0)
+    r = spi.xfer2([8+0 << 4])
     print(r)
-    print(((r[1] & 3) << 8) + r[2])
+    #print(((r[1] & 3) << 8) + r[2])
 
 
 spi.close()
