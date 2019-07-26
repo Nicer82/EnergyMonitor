0581f61ee29badbebf333fc04274ab97732ceac4
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-07-26 10:31:01 +0200

    Update testcvmcp.py

--- testcvmcp.py
+++ testcvmcp.py
@@ -93,7 +93,7 @@ spi = spidev.SpiDev()
 spi.open(0,0)
 
 for i in range(800):
-    r = spi.xfer3([1, 8 + 1 << 4, 0],50000)
+    r = spi.xfer3([1, 8 + 1 << 4, 0],50000,0)
     print(r)
     
         
