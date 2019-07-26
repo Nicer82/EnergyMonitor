43c3689c49f96a18bf3cdd6dec0b9466ae81bb2a
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-07-26 16:47:20 +0200

    Update testcvmcp.py

--- testcvmcp.py
+++ testcvmcp.py
@@ -61,10 +61,10 @@ def readadc(channels):
 
     end = time.perf_counter()
 
-    for datasample in data:
-        print("{};{}".format(datasample[0],datasample[1]))
+    for i in data[0]:
+        print("{};{}".format(data[0][i],data[1][i]))
 
-    print("Reads: {}, Performance: {} sps, Requested time: {} ms, Actual time: {} ms".format(len(data),len(data)/(end-start),1000/AC_FREQUENCY*ADC_ACWAVESTOREAD,(end-start)*1000))
+    print("Reads: {}, Performance: {} sps, Requested time: {} ms, Actual time: {} ms".format(len(data[0]),len(data[0])/(end-start),1000/AC_FREQUENCY*ADC_ACWAVESTOREAD,(end-start)*1000))
     
     return data
 
