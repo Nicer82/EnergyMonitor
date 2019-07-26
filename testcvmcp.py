b863f3a2f6597a51288ed79badd65948067c7664
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-07-26 13:02:19 +0200

    Update testcvmcp.py

--- testcvmcp.py
+++ testcvmcp.py
@@ -97,6 +97,7 @@ nextRead = time.perf_counter()
 for i in range(ADC_SAMPLESPERWAVE*ADC_ACWAVESTOREAD):
     nextRead += 1/(ADC_SAMPLESPERWAVE*AC_FREQUENCY)
     delay = round(max([0,nextRead-time.perf_counter()])*1000000)
+    print(delay)
     data.append(spi.xfer2([6+((4&chan)>>2),(3&chan)<<6,0],100000,round(delay*1000000)))
     
 for d in data:
