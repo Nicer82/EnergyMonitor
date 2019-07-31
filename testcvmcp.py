5ee501e04812a3b9042671e4757def100e530a6b
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-07-31 11:09:37 +0200

    Update testcvmcp.py

--- testcvmcp.py
+++ testcvmcp.py
@@ -12,7 +12,7 @@ AC_FREQUENCY = 50
 
 CHANNELS = [0,1,2,3,4,5]
 CALIBRATIONFACTOR = [0.004047,0.004047,0.004047]
-
+CALIBRATIONFACTOR_V = [1,1,1]
 def rootmeansquare(values):
     # RMS = SQUARE_ROOT((values[0]² + values[1]² + ... + values[n]²) / LENGTH(values))
     sumsquares = 0.0
@@ -95,14 +95,22 @@ while(True):
         
     ### Calculate the power
     power = []
+    voltage = []
+    current = []
+    
     for channel in [0,2,4]:
         powerdata = []                            
+        li = round(channel/2)
+        
         for reading in range(len(data[channel])):
             powerdata.append(data[channel][reading] * data[channel+1][reading])
         
-        power.append(statistics.mean(powerdata)*CALIBRATIONFACTOR[round(channel/2)]);
-        print("L{}: Power: {} W".format(round(channel/2)+1,round(power[round(channel/2)])))
+        power.append(statistics.mean(powerdata)*CALIBRATIONFACTOR[li]);
+        voltage.append(rootmeansquare(data[channel+1])*CALIBRATIONFACTOR_V[li])
+        current.append(power[li]/voltage[li])
+        
+        print("L{}: Current: {} A, Voltage: {} V, Power: {} W".format(li+1,round(current[li],3),round(voltage[li],1), round(power[li])))
     
-    print("Total: Power: {} W".format(round(sum(power))))
+    print("Total: Current: {} A, Voltage: {} V, Power: {} W".format(,round(sum(current),3),round(statistics.mean(voltage),1),round(sum(power))))
 
 spi.close()
