b4f3655e9ec48a2935223f677345985df0edeffe
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-08-04 12:38:50 +0200

    Create state.py

---
+++ API/state.py
@@ -0,0 +1,17 @@
+import json
+from flask import Flask
+from flask_restful import Resource, reqparse ,Api
+
+app = Flask(__name__)
+api = Api(TGS)
+
+with open('/home/pi/EnergyMonitorRAM/state.json') as json_data:
+    state = json.load(json_data)
+
+class State(Resource):
+    def get(self):
+        with open('/home/pi/EnergyMonitorRAM/state.json') as json_data:
+            state = json.load(json_data)
+        return state, 200
+
+api.add_resource(State, "/state")
