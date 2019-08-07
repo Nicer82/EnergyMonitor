6fa494499fd27ff181a59a9c9e23034f457b67d7
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-08-07 10:05:37 +0200

    Update state.py

--- API/state.py
+++ API/state.py
@@ -1,9 +1,10 @@
+#! /usr/bin/python3
 import json
 from flask import Flask
 from flask_restful import Resource, reqparse ,Api
 
 app = Flask(__name__)
-api = Api(TGS)
+api = Api(app)
 
 with open('/home/pi/EnergyMonitorRAM/state.json') as json_data:
     state = json.load(json_data)
