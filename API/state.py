ec3611dcad47e33102905fec7ac866060780e79a
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-08-15 11:13:00 +0200

    Update state.py

--- API/state.py
+++ API/state.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python3
 import json
+import socket
 from flask import Flask
 from flask_restful import Resource, reqparse ,Api
 
@@ -18,4 +19,11 @@ class State(Resource):
 api.add_resource(State, "/state")
 
 if __name__ == "__main__":
-    app.run(host='192.168.1.55',debug=False,port=8080)
+    # Read configuration
+    with open('/home/pi/EnergyMonitor/config.json') as json_data:
+        config = json.load(json_data)
+    
+    # Get the current IP
+    ip = [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]
+    
+    app.run(host=ip,debug=False,port=config["Api"]["Port"])
