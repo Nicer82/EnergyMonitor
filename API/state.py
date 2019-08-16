c5938a3967887f1ea0841084d71d9265239df03f
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-08-16 17:27:58 +0200

    Update state.py

--- API/state.py
+++ API/state.py
@@ -1,13 +1,21 @@
 #! /usr/bin/python3
 import json
 import socket
+import time
 from flask import Flask, request
 from flask_restful import Resource, reqparse ,Api
+
+# Read configuration
+with open('/home/pi/EnergyMonitor/config.json') as json_data:
+    config = json.load(json_data)
 
 app = Flask(__name__)
 api = Api(app)
 
 statedata = []
+volumedata = []
+volumestart = time.time() DIV config["Api"]["VolumeDataSeconds"] * config["Api"]["VolumeDataSeconds"]
+volumeend = volumestart + config["Api"]["VolumeDataSeconds"]
 
 class State(Resource):
     def get(self, point):
@@ -15,9 +23,6 @@ class State(Resource):
             if(point == pointdata["point"]):
                 return pointdata, 200
         return "point not found", 404
-        #with open('/home/pi/EnergyMonitorRAM/state.json') as json_data:
-        #    state = json.load(json_data)
-        #return state, 200
     def put(self, point):
         # fetch the values from the post data
         newpointdata = json.loads(request.data.decode('ascii'))
@@ -41,32 +46,12 @@ class State(Resource):
                 return pointdata, 200
                 
         # insert in case the point doesn't already exist
-        #pointdata = {
-        #    "point": point,
-        #    "time": args["time"],
-        #    "l1_current": args["l1_current"],
-        #    "l1_voltage": args["l1_voltage"],
-        #    "l1_power": args["l1_power"],
-        #    "l2_current": args["l2_current"],
-        #    "l2_voltage": args["l2_voltage"],
-        #    "l2_power": args["l2_power"],
-        #    "l3_current": args["l3_current"],
-        #    "l3_voltage": args["l3_voltage"],
-        #    "l3_power": args["l3_power"],
-        #    "total_current": args["total_current"],
-        #    "total_voltage": args["total_voltage"],
-        #    "total_power": args["total_power"]
-        #}
         statedata.append(newpointdata)
         return pointdata, 201
                 
 api.add_resource(State, "/state/<string:point>")
 
 if __name__ == "__main__":
-    # Read configuration
-    with open('/home/pi/EnergyMonitor/config.json') as json_data:
-        config = json.load(json_data)
-    
     # Get the current IP
     ip = [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]
     
