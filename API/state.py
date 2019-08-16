ff780d08db37998b0a7be07458580c63c6ce6041
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-08-16 15:00:58 +0200

    Update state.py

--- API/state.py
+++ API/state.py
@@ -10,17 +10,18 @@ api = Api(app)
 statedata = []
 
 class State(Resource):
-    def get(self, point):
-        if(point != ''):
-            for pointdata in statedata:
-                if(point == pointdata["point"]):
-                    return pointdata, 200
-            return "point not found", 404
+    def get(self):
         return statedata, 200
+    def get(self, point):
+        for pointdata in statedata:
+            if(point == pointdata["point"]):
+                return pointdata, 200
+        return "point not found", 404
         #with open('/home/pi/EnergyMonitorRAM/state.json') as json_data:
         #    state = json.load(json_data)
         #return state, 200
 
+api.add_resource(State, "/state")
 api.add_resource(State, "/state/<string:point>")
 
 if __name__ == "__main__":
