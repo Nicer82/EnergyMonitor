35c08af2530e95970a012380309995b7a0e2bf55
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-08-16 16:43:00 +0200

    Update state.py

--- API/state.py
+++ API/state.py
@@ -20,63 +20,44 @@ class State(Resource):
         #return state, 200
     def put(self, point):
         # fetch the values from the post data
-        
         newpointdata = json.loads(request.data.decode('ascii'))
-        print(newpointdata)
-        
-        parser = reqparse.RequestParser()
-        parser.add_argument("point")
-        parser.add_argument("time")
-        parser.add_argument("l1_current")
-        parser.add_argument("l1_voltage")
-        parser.add_argument("l1_power")
-        parser.add_argument("l2_current")
-        parser.add_argument("l2_voltage")
-        parser.add_argument("l2_power")
-        parser.add_argument("l3_current")
-        parser.add_argument("l3_voltage")
-        parser.add_argument("l3_power")
-        parser.add_argument("total_current")
-        parser.add_argument("total_voltage")
-        parser.add_argument("total_power")
-        args = parser.parse_args()
         
         # update in case the point already exists
         for pointdata in statedata:
             if(point == pointdata["point"]):
-                pointdata["time"] = args["time"]
-                pointdata["l1_current"] = args["l1_current"]
-                pointdata["l1_voltage"] = args["l1_voltage"]
-                pointdata["l1_power"] = args["l1_power"]
-                pointdata["l2_current"] = args["l2_current"]
-                pointdata["l2_voltage"] = args["l2_voltage"]
-                pointdata["l2_power"] = args["l2_power"]
-                pointdata["l3_current"] = args["l3_current"]
-                pointdata["l3_voltage"] = args["l3_voltage"]
-                pointdata["l3_power"] = args["l3_power"]
-                pointdata["total_current"] = args["total_current"]
-                pointdata["total_voltage"] = args["total_voltage"]
-                pointdata["total_power"] = args["total_power"]
+                pointdata["time"] = newpointdata["time"]
+                pointdata["l1_current"] = newpointdata["l1_current"]
+                pointdata["l1_voltage"] = newpointdata["l1_voltage"]
+                pointdata["l1_power"] = newpointdata["l1_power"]
+                pointdata["l2_current"] = newpointdata["l2_current"]
+                pointdata["l2_voltage"] = newpointdata["l2_voltage"]
+                pointdata["l2_power"] = newpointdata["l2_power"]
+                pointdata["l3_current"] = newpointdata["l3_current"]
+                pointdata["l3_voltage"] = newpointdata["l3_voltage"]
+                pointdata["l3_power"] = newpointdata["l3_power"]
+                pointdata["total_current"] = newpointdata["total_current"]
+                pointdata["total_voltage"] = newpointdata["total_voltage"]
+                pointdata["total_power"] = newpointdata["total_power"]
                 return pointdata, 200
                 
         # insert in case the point doesn't already exist
-        pointdata = {
-            "point": point,
-            "time": args["time"],
-            "l1_current": args["l1_current"],
-            "l1_voltage": args["l1_voltage"],
-            "l1_power": args["l1_power"],
-            "l2_current": args["l2_current"],
-            "l2_voltage": args["l2_voltage"],
-            "l2_power": args["l2_power"],
-            "l3_current": args["l3_current"],
-            "l3_voltage": args["l3_voltage"],
-            "l3_power": args["l3_power"],
-            "total_current": args["total_current"],
-            "total_voltage": args["total_voltage"],
-            "total_power": args["total_power"]
+        #pointdata = {
+        #    "point": point,
+        #    "time": args["time"],
+        #    "l1_current": args["l1_current"],
+        #    "l1_voltage": args["l1_voltage"],
+        #    "l1_power": args["l1_power"],
+        #    "l2_current": args["l2_current"],
+        #    "l2_voltage": args["l2_voltage"],
+        #    "l2_power": args["l2_power"],
+        #    "l3_current": args["l3_current"],
+        #    "l3_voltage": args["l3_voltage"],
+        #    "l3_power": args["l3_power"],
+        #    "total_current": args["total_current"],
+        #    "total_voltage": args["total_voltage"],
+        #    "total_power": args["total_power"]
         }
-        statedata.append(pointdata)
+        statedata.append(newpointdata)
         return pointdata, 201
                 
 api.add_resource(State, "/state/<string:point>")
