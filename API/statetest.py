0d03db479263bef4da29bc219b930abc3d9e7e56
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-08-17 11:32:02 +0200

    Update statetest.py

--- API/statetest.py
+++ API/statetest.py
@@ -1,3 +1,21 @@
-from state import State
+import state
+import time
 
 s = State(0)
+
+statepointdata = {
+  "time": time.time(),
+  "l1_current": 1.123,
+  "l1_voltage": 240.1,
+  "l1_power": 269.6323,
+  "l2_current": 1.321
+  "l2_voltage": 240.2
+  "l2_power": 317.3042
+  "l3_current": 1.231
+  "l3_voltage": 240.3
+  "l3_power": 295.8093
+  "total_current": 3.675
+  "total_voltage": 240.2
+  "total_power": 882.7458
+}
+s.registerState('test',statepointdata)
