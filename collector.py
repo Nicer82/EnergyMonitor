e7832bd37fc4b66056a29b21f2c80415507be71d
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-08-22 11:15:53 +0200

    Update collector.py

--- collector.py
+++ collector.py
@@ -1,77 +1,9 @@
 import json
 import time
 import statistics
-import math
-import spidev
 import logging
-import subprocess
 import emlib
 from datetime import datetime
-
-def run_process(cmd):
-    result = None
-    try:
-        result = subprocess.check_output(cmd, shell=True)
-    except subprocess.CalledProcessError:
-        # We reach this point if the process returns a non-zero exit code.
-        result = b''
-
-    return result
-
-def rootmeansquare(values):
-    # RMS = SQUARE_ROOT((values[0]² + values[1]² + ... + values[n]²) / LENGTH(values))
-    sumsquares = 0.0
-
-    for value in values:
-        sumsquares = sumsquares + (value)**2 
-
-    if len(values) == 0:
-        rms = 0.0
-    else:
-        rms = math.sqrt(float(sumsquares)/len(values))
-
-    return rms
-
-def normalize(values):
-    avg = round(statistics.mean(values))
-
-    # Substract the mean of every value to set the mean to 0
-    for i in range(len(values)):
-        values[i] -= avg
-        
-    return values
-
-def readadc(channels,samplesperwave,wavestoread,frequency):
-    data = []
-    
-    for i in channels:
-        data.append([])
-        
-    start = time.perf_counter()
-    nextRead = start
-
-    for i in range(samplesperwave*wavestoread):
-        nextRead += 1/(samplesperwave*frequency)
-
-        # Read channels
-        for ci in range(len(channels)):
-            # Add a delay on the last channel to match timings. This is way more accurate than time.sleep() because it works up to the microsecond.
-            if(ci == len(channels)-1):
-                delay = max([0,round((nextRead-time.perf_counter())*1000000)]) 
-            else:
-                delay = 0
-
-            response = spi.xfer2([6+((4&channels[ci])>>2),(3&channels[ci])<<6,0],2000000,delay)
-            data[ci].append(((response[1] & 15) << 8) + response[2])
-
-    end = time.perf_counter()
-
-    #for i in range(len(data[0])):
-    #    print("{};{}".format(data[0][i],data[1][i]))
-
-    #print("Reads: {}, Performance: {} sps, Requested time: {} ms, Actual time: {} ms".format(len(data[0]),len(data[0])/(end-start),1000/AC_FREQUENCY*ADC_ACWAVESTOREAD,(end-start)*1000))
-    
-    return data
 
 # Read configuration
 with open('/home/pi/EnergyMonitor/config.json') as json_data:
@@ -87,15 +19,14 @@ logging.basicConfig(filename=logFileName,
                     level=logging.ERROR, 
                     format='%(asctime)s %(levelname)s %(message)s')
 
-# Create the SPI
-spi = spidev.SpiDev()
-spi.open(0,0)
+# Create the reader object
+reader = emlib.AdcReader()
 
 # Infinite loop
 while(True):
     try:
         ### Read the channels
-        data = readadc(channels,config["Collector"]["SamplesPerWave"],config["Collector"]["WavesToRead"],config["Collector"]["Frequency"])
+        data = reader.readSineWave(channels,config["Collector"]["SamplesPerWave"],config["Collector"]["WavesToRead"],config["Collector"]["Frequency"])
 
         ### Normalize the captured data
         for i in range(len(data)):
@@ -144,9 +75,8 @@ while(True):
         print(e)
         logging.exception("Exception occurred, waiting 10 seconds before continueing")
 
-        # Reset the SPI
-        spi = spidev.SpiDev()
-        spi.open(0,0)
-
         # Wait 10 seconds to avoid flooding the error log too much
         time.sleep(10)
+
+        # Reset the reader
+        reader = emlib.AdcReader()
