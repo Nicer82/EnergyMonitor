60173c7d4836c2ff949e3afbce91f07a0e24e12f
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-08-30 15:17:53 +0200

    Update test.py

--- test.py
+++ test.py
@@ -27,22 +27,3 @@ while(True):
         #print("Channel {} before normalize: Reads: {}, Min: {}, Max: {}".format(i,len(data[i]),min(data[i]),max(data[i])))
         data[i] = emlib.normalize(data[i])
         #print("Channel {} after normalize: Reads: {}, Min: {}, Max: {}".format(i,len(data[i]),min(data[i]),max(data[i])))
-        
-    ### Calculate the power
-    power = []
-    voltage = []
-    current = []
-    
-    for channel in [0,2,4]:
-        powerdata = []                            
-        li = round(channel/2)
-        
-        for reading in range(len(data[channel])):
-            powerdata.append(data[channel][reading] * data[channel+1][reading])
-        
-        power.append(statistics.mean(powerdata)*CALIBRATIONFACTOR[li]);
-        voltage.append(emlib.rootmeansquare(data[channel+1])*CALIBRATIONFACTOR_V[li])
-        current.append(power[li]/voltage[li])
-        print("L{}: Current: {} A, Voltage: {} V, Power: {} W".format(li+1,round(current[li],3),round(voltage[li],1), round(power[li])))
-    
-    print("Total: Current: {} A, Voltage: {} V, Power: {} W".format(round(sum(current),3),round(statistics.mean(voltage),1),round(sum(power))))
