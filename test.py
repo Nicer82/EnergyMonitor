46a03d545e509ddeb63c931c5a036a4b75a81f9d
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-08-30 16:00:18 +0200

    Update test.py

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ CHANNELS = [0]
 #reader = emlib.AdcReader()
 
 spi = spidev.SpiDev()
-spi.open(0,1)
+spi.open(1,0)
 channel=0
 response = spi.xfer2([6+((4&channel)>>2),(3&channel)<<6,0], 1000000)
 print(response)
