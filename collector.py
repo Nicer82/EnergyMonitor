bf1a126a8a4fca2626f8b8247e72a3f12f5f86e8
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-09-01 14:04:51 +0200

    Update collector.py

--- collector.py
+++ collector.py
@@ -82,6 +82,8 @@ while(True):
         jsondata['point'] = config["Collector"]["Point"]
         jsondata['time'] = time.time()
         
+        f= open("/home/pi/dstemp/EnergyMonitor.log","a+") #temp
+        
         for wirecolor in config["Collector"]["CurrentChannels"]:
             powerdata = []                            
 
@@ -91,6 +93,7 @@ while(True):
                 voltageData = data[voltageidxs[wirecolor]]
 
             for reading in range(len(data[currentidxs[wirecolor]])):
+                f.write('{};{};{};{}'.format(jsondata['time'],wirecolor,data[currentidxs[wirecolor]][reading] * voltageData[reading])) # temp
                 powerdata.append(data[currentidxs[wirecolor]][reading] * voltageData[reading])
             
             wirepower = statistics.mean(powerdata)*config["Collector"]["CalibrationFactor_Power"]
@@ -109,6 +112,7 @@ while(True):
             jsondata[wirecolor]['voltage'] = round(wirevoltage,1)
             jsondata[wirecolor]['power'] = round(wirepower,4)
 
+        f.close() # temp
         jsondata['current'] = round(sum(current),3)
         jsondata['voltage'] = round(statistics.mean(voltage),1)
         jsondata['power'] = round(sum(power))
