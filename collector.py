19ca685aee00e856766b74ea45341cf059b0cafb
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-09-01 14:47:13 +0200

    Update collector.py

--- collector.py
+++ collector.py
@@ -114,7 +114,7 @@ while(True):
         jsondata['power'] = round(sum(power))
         
         # post the new state to the state device
-        #statePostQueue.put(jsondata.copy())
+        statePostQueue.put(jsondata.copy())
         #if(not statePostThread or not statePostThread.is_alive()):
         #    statePostThread = threading.Thread(target=postStates)
         #    statePostThread.start()
