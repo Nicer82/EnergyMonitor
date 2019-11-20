e3772b2848b1ac20d67c607a4edf41403caae905
Nicer82 <46420661+Nicer82@users.noreply.github.com>
2019-11-20 10:20:27 +0100

    Update collector.py

--- collector.py
+++ collector.py
@@ -1,4 +1,5 @@
 #! /usr/bin/python3
+import os
 import threading
 import socket
 import json
@@ -28,7 +29,7 @@ def postStates():
                 time.sleep(5) # wait 5 seconds after an error to not overwhelm the attempts
 
 # Read configuration
-with open('config.json') as json_data:
+with open(os.path.join(os.getcwd(),'config.json')) as json_data:
     config = json.load(json_data)
     channels = []
     currentidxs = {}
